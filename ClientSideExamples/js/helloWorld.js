d5da90551a56d78f1b8e1f887704a2e6e328b8d3
qschultz <quinlan@zmulls.com>
2018-06-19 15:49:29 -0400

    CSS, Bootstrap, and JS excercises

---
+++ ClientSideExamples/js/helloWorld.js
@@ -0,0 +1,24 @@
+/* Hello World */
+console.log('Hello World');
+console.log("1 == '1'=>", 1 == '1');
+console.log("1 === '1'=>", 1 === '1');
+console.log("3 == '3.0'=>", 3 == '3.0');
+console.log('3.0 == "3"=>', 3.0 == "3");
+console.log("3.0 == 3=>", 3.0 == '3');
+console.log("3 === '3.0'=>", 3 === '3.0');
+console.log('0 == false =>', 0 == false);
+console.log('"" == false =>', "" == false);
+console.log('0 === false =>', 0 === false);
+console.log('"" === false =>', "" === false);
+console.log('null == null =>', null == null);
+console.log('null == undefined =>', null == undefined);
+console.log('undefined == undefined =>', undefined == undefined);
+console.log('null === undefined =>', null === undefined);
+console.log('null === null =>', null === null);
+console.log('null == false =>', null == false);
+if ('William') {
+    console.log('"William" is truthy')
+}
+console.log('typeof "William" =>', typeof "William")
+console.log('typeof infinity =>', typeof Infinity)
+console.log('typeof NaN =>', typeof NaN)
