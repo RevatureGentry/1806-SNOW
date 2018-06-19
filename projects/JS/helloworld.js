c5634c50d7b2bbfede28086098151f1df4d6a633
Jared Boese <jgboese@mavs.coloradomesa.edu>
2018-06-19 16:06:29 -0400

    Notes from 6/19

---
+++ projects/JS/helloworld.js
@@ -0,0 +1,15 @@
+/*Hello World */
+console.log("Hello World");
+console.log("1 == '1'  => ", 1 == '1');
+console.log("1 === '1'=> ",1==='1');
+console.log("0 == false =>", 0==false);
+console.log("0 === false=>" ,0===false);
+console.log("null == null=> ", null == null);
+console.log("null == undefined =>", null == undefined);
+if ("Jared"){
+    console.log("'Jared' is truthy");
+}
+if([]){
+    console.log("[] is truthy");
+}
+console.log( "Typeof 'Jared' =>", typeof('Jared'));
