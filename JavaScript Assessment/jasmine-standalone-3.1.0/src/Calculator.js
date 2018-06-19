77c55586638b58d132b3f008b016161f9698e1b4
thomaslai113 <thomas.lai113@gmail.com>
2018-06-19 19:21:35 -0400

    Added safeties, completed unit tests successfully

--- JavaScript Assessment/jasmine-standalone-3.1.0/src/Calculator.js
+++ JavaScript Assessment/jasmine-standalone-3.1.0/src/Calculator.js
@@ -4,7 +4,6 @@ let Calculator = function() {
     this.sum = function(array) {
         var answer = 0;
 		for(var i=0; i<array.length;i++){
-			console.log(typeof array[i]);
 			if(typeof array[i] != 'number')
 				throw new Error();
 			answer+=array[i];
@@ -14,9 +13,9 @@ let Calculator = function() {
 
     /* Provide an implementation that finds the factorial of value (value!) */
     this.factorial = function(value) {
-		if(typeof value !== 'number')
+        if(typeof value !== 'number')
 			throw new Error('must be a number');
-		else if(value <= 0)
+		else if(value < 0)
 			throw new Error('no negative values allowed');
 		
         var answer = 1;
@@ -29,24 +28,34 @@ let Calculator = function() {
     /* Provide an implementation that returns true if a number is odd, and false if the number is even */
     /* CONDITION: You may not use the modulus (%) operator */
     this.isOdd = function(value) {
-        var v = value/2;
-		var x = value/v;
-		x = x-2;
-		if(x){
-			return false;
+		if(typeof value !== 'number')
+			throw new Error('must be a number');
+		var v = []
+        v = value/2 + '';
+		if(v[v.length-2] == ".")
+		{
+			if(v[v.length-1]=="5")
+			{
+				return true;
+			}
+			else {return false;}
 		}
 		else{
-			return true;
+			return false;
 		}
     };
 
     /* Provide an implementation that finds the nth term in the Fibonacci Sequence */
     /* HINT: The first two terms are 1 */
     this.termInFibonacciSequence =  function(n) {
+		if(typeof n !== "number")
+			throw new Error("muyst be a number");
+		else if(n <= 0)
+			throw new Error("cannot be negative");
         var one = 1;
-	var two = 1;
-	var three;
-	for(var i = 2; i< value;i++)
+		var two = 1;
+		var three;
+	for(var i = 2; i< n;i++)
 	{
 		three = one+two;
 		one = two;	
--- JavaScript Assessment/jasmine-standalone-3.1.0/src/StringManipulator.js
+++ JavaScript Assessment/jasmine-standalone-3.1.0/src/StringManipulator.js
@@ -3,10 +3,19 @@ function StringManipulator() {
     /* Provide an implementation that returns true if a string is a palindrome, and false if the string is not */
     /* CONDITION: you may not use String.prototype.reverse() */
     this.isPalindrome = function(value) {
-		if(typeof value !== "string")
-			return new Error("must be a string");
+		if(typeof value !== 'string'){
+			throw new Error("must be a string");
+		}
+		value.trim
         var j = value.length-1;
 		for(var i=0;i<value.length/2;i++){
+			if(value[i] == " "){
+				i++;
+			}
+			if(value[j] == " ")
+			{
+				j--;
+			}
 		if(value[i] !== value[j]){
 			return false;
 		}
@@ -21,7 +30,7 @@ function StringManipulator() {
     /* CONDITION: you may only use String.prototype.charAt() */
     this.reverseString = function(value) {
 		if(typeof value !== "string")
-			return new Error("must be a string");
+			throw new Error("must be a string");
         var j = value.length-1;
 		var i = 0;
 		var pop=[];
@@ -35,7 +44,7 @@ function StringManipulator() {
     /* Provide an implementation that counts and returns the occurence of the letter "B" in a string */
     this.countBs = function(value) {
 		if(typeof value !== "string")
-			return new Error("must be a string");
+			throw new Error("must be a string");
 	var bee = 'B';
 	var count = 0;
 	for(var i = 0; i<value.length;i++){
@@ -50,14 +59,25 @@ function StringManipulator() {
     /* Occurence of the letter 'char' in the String 'value' */
     this.countCharInString = function(value, char) {
 		if(typeof value !== "string")
-			return new Error("must be a string");
-		else if(char.length > 1)
-			return new Error("must be a char");
-var bee = char;
+			throw new Error("must be a string");
+		else if(typeof char !== "string")
+			throw new Error("must be a string");
+	var bee = char;
 	var count = 0;
 	for(var i = 0; i<value.length;i++){
-		if(value[i] == bee){
-			count++;
+		for(var j = 0; j< bee.length;j++){
+			console.log(value[i]);
+			console.log(bee[j]);
+			if(value[i] === bee[j]){
+				if(j == bee.length-1)
+				{
+					count++;
+				}
+				i++;
+			}
+			else{
+				break;
+			}
 		}
 	}
 	return count;
