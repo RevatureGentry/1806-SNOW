6625816901c2d9e62c541a83b3e2d0e776fcbaa2
kchang0381 <kchang0381@gmail.com>
2018-06-19 15:58:45 -0400

    Added javascript example

---
+++ helloworld.js
@@ -0,0 +1,7 @@
+/*Hello World*/
+console.log("Hello world");
+console.log("1 == '1' =>", 1 == '1');
+console.log("1 === '1' =>", 1 === '1');
+console.log("3 == '3.0' =>", 3 == '3.0');
+console.log("'' == false =>", '' == false);
+console.log("typeof 'asdf' =>", typeof 'asdf');
