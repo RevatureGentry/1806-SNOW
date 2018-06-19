5d49fefcafac877af42979179f9da2db20d3637f
mgallo96 <michaelgallo96@gmail.com>
2018-06-19 16:08:17 -0400

    ClientSideExamples

---
+++ ClientSideExamples/js/helloWorld.js
@@ -0,0 +1,28 @@
+/* Hello World */
+console.log('Hello World');
+console.log("1 == '1' =>", 1 == '1');
+console.log("1 === '1' =>", 1 === '1');
+console.log("3 == '3.0' =>", 3 == '3.0');
+console.log("3.0 == '3' =>", 3.0 == '3');
+console.log("3.0 == 3 =>", 3.0 == 3);
+console.log("3 === '3.0' =>", 3 === '3.0');
+console.log("0 == false =>", 0 == false);
+console.log("'' == false =>", '' == false);
+console.log("0 === false =>", 0 === false);
+console.log("'' === false =>", '' === false);
+console.log("null == null =>", null == null);
+console.log("null == undefined =>", null == undefined);
+console.log("undefined == undefined =>", undefined == undefined);
+console.log("undefined == null =>", undefined == null);
+if('William'){
+    console.log("'William' is truthy");
+}
+if(42){
+    console.log("42 is truthy");
+}
+if([]){
+    console.log("[] is truthy");
+}
+console.log('typeof "William" =>', typeof "William");
+console.log('typeof Infinity =>', typeof Infinity);
+console.log('typeof NaN =>', typeof NaN);
