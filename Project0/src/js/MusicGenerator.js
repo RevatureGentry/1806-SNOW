ec8dbd52944eee20e2e3f5a79ffe7017daf4c239
chenhowa <hchen7913@gmail.com>
2018-06-20 10:54:05 -0400

    Work on project0

---
+++ Project0/src/js/MusicGenerator.js
@@ -0,0 +1,11 @@
+
+// The purpose of this class is to convert 
+// strings (likely read from files) into musical notes to be played
+// in the browser
+class MusicGenerator {
+
+
+}
+
+
+export default MusicGenerator;
