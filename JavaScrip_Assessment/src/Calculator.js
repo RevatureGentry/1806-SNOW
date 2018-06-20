2a9233c2c27f7ad80bf192e6330e1651a2bad43a
chenhowa <hchen7913@gmail.com>
2018-06-19 21:02:15 -0400

    Redownload Jasmine, implement all functions

--- JavaScrip_Assessment/src/Calculator.js
+++ JavaScrip_Assessment/src/Calculator.js
@@ -2,23 +2,82 @@ let Calculator = function() {
 
     /* Provide an implementation that sums the elements in an array */
     this.sum = function(array) {
-        
+        if(array.length === 0) {
+            return 0;
+        }
+
+        if(typeof(array[0]) !== 'number') {
+            throw new Error("array is not numeric");
+        }
+
+        let sum = array.reduce((acc, currentValue) => {
+            if(typeof(currentValue) !== 'number') {
+                throw new Error("array is not numeric");
+            }
+            return acc + currentValue;
+        });
+
+        return sum;
     };
 
     /* Provide an implementation that finds the factorial of value (value!) */
     this.factorial = function(value) {
-        
+        if(typeof(value) !== 'number') {
+            throw Error("not a number!");
+        }
+
+        if(value < 0) {
+            throw new Error("negative value");
+        }
+
+        if(value === 1) {
+            return 1;
+        }
+
+        let product = 1;
+        for(let i = value; i > 0; i--) {
+            product = product * i;
+        }
+
+        return product;
     }
 
     /* Provide an implementation that returns true if a number is odd, and false if the number is even */
     /* CONDITION: You may not use the modulus (%) operator */
     this.isOdd = function(value) {
-        
+        if(typeof(value) !== 'number') {
+            throw new Error("not a number");
+        }
+
+        //I don't really want to mess with 'this'.
+        if(value < 0) {
+            value *= -1;
+        }
+
+        return (value & 1) === 1;
     }
 
     /* Provide an implementation that finds the nth term in the Fibonacci Sequence */
     /* HINT: The first two terms are 1 */
     this.termInFibonacciSequence =  function(n) {
-        
+        if(typeof(n) !== 'number') {
+            throw new Error("not a number!");
+        }
+
+        if(n < 0) {
+            throw new Error("can't calculate for negative values");
+        }
+
+        // 0 is a dummy value to allow the index to start at 1
+        let memo = [0, 1, 1];
+        if(n === 1 || n === 2) {
+            return 1;
+        }
+
+        for(let i = 3; i <= n; i++) {
+            memo.push(memo[i-1] + memo[i-2]);
+        }
+
+        return memo[n];
     };
 };
--- JavaScrip_Assessment/src/StringManipulator.js
+++ JavaScrip_Assessment/src/StringManipulator.js
@@ -3,23 +3,84 @@ function StringManipulator() {
     /* Provide an implementation that returns true if a string is a palindrome, and false if the string is not */
     /* CONDITION: you may not use String.prototype.reverse() */
     this.isPalindrome = function(value) {
+        if(typeof(value) !== 'string') {
+            throw new Error("input is not a string");
+        }
+
+        //Use array as stack to see if string is palindrome. Other option is 
+        // to iterate from both ends.
+        if(value.length === 0) {
+            return true;
+        }
+
+        let str = value.replace(/\s/g, "");
+        for(let i = 0; i < Math.floor(str.length / 2); i++) {
+            if(str.charAt(i) !== str.charAt(str.length - 1 - i)) {
+                return false;
+            }
+        }
+
+        return true;
         
     }
 
     /* Provide an implementation that reverses a string */
     /* CONDITION: you may only use String.prototype.charAt() */
     this.reverseString = function(value) {
-        
+        if(typeof(value) !== 'string') {
+            throw new Error("input is not a string");
+        }
+
+        if(value.length === 0) {
+            return "";
+        }
+
+        let result = "";
+        for(let i = value.length - 1; i >= 0; i--) {
+            result += value.charAt(i);
+        }
+
+        return result;
     }
 
     /* Provide an implementation that counts and returns the occurence of the letter "B" in a string */
     this.countBs = function(value) {
+        if(typeof(value) !== 'string') {
+            throw new Error("input is not a string");
+        }
 
+        let count = 0;
+        for(let i = 0; i < value.length; i++) {
+            if(value.charAt(i) === "B") {
+                count += 1;
+            }
+        }
+
+        return count;
     }
 
     /* An abstraction of the previous function, provide an implementation that counts and returns the  */
     /* Occurence of the letter 'char' in the String 'value' */
     this.countCharInString = function(value, char) {
+        if(typeof(value) !== 'string') {
+            throw new Error("value is not a string");
+        }
 
+        if(typeof(char) !== 'string') {
+            throw new Error('char is not a string');
+        }
+
+        if(typeof(value) !== 'string') {
+            throw new Error("input is not a string");
+        }
+
+        let count = 0;
+        for(let i = 0; i < value.length; i++) {
+            if(value.substring(i, i + char.length) === char) {
+                count += 1;
+            }
+        }
+
+        return count;
     }
 }
