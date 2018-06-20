66896964a983b135737e8bbd31c252a18e630668
BraydenMitchell <braydenwithawhy@gmail.com>
2018-06-20 13:32:17 -0400

    Redownloaded/Resubmitted Jasmine.js due to bugs. Finished most of calculator.js. More notes in helloWorld.js/.html.

--- ClientSideExamples/js/helloWorld.js
+++ ClientSideExamples/js/helloWorld.js
@@ -28,3 +28,143 @@ if (42) {
 console.log("typeof 'William' => " + (typeof 'William'));
 console.log("typeof Infinity => " + (typeof Infinity));
 console.log("typeof NaN => " + (typeof NaN));
+
+//Scope
+var globalScope = "Ohai, I be a global scope variable.."; //This variable can be accessed anywhere in our program.
+
+function createLocalScope() {
+    console.log("Inside createLocalScope().");
+    var localScope = "Oh, hey. I'm a local scope variable."; //This is accessible anywhere in the function.
+    console.log(localScope);
+    console.log(globalScope);
+    if ('InLocalScope') {
+        var blockScope = "Oh...I'm a block scope variable."; //This is only accessible in the block it was initialized. 
+        console.log(blockScope);
+    }
+}
+
+function pitfallsOfVar() { //Demonstrates why var is bad to rely on.
+    var testVar = 'Instantiated on Line 51.';
+    console.log(testVar);
+    var testVar = 'Instantiated on Line 53.';
+    console.log(testVar);
+}
+
+function whyLetRocks() { //Demonstrates error that occurs when using let variables.
+    let testVar = 'Line 60.';
+    console.log(testVar);
+    //let testVar = 'Line 62.';
+    //console.log(testVar);
+}
+console.log(globalScope);
+
+//Function Invoking.
+createLocalScope(); //Invoke creatLocalScope().
+//pitfallsOfVar(); //Invoke pitfallsOfVar().
+//whyLetRocks(); //Invoke whyLetRocks().
+
+//Three ways Functions can be assigned in JavaScript:
+
+//1. Functions as values
+let anonHello = function(name) {
+    /*
+    Template Literal Example: allows you to write a string and directly inject expressions/variables into a string. 
+        "Without the " + name + " additional work"
+    */
+    console.log(`Hello, ${name} (Anonymous)`); //${} is how you inject expressions into strings. `` is how you set it up.
+}
+console.log("anonHello:\n", anonHello);
+anonHello("Burden");
+
+//2. Declared Functions.
+function declaredHello(name) {
+    console.log(`Hello, ${name} (Declared)`);
+}
+console.log("declaredHello:\n", declaredHello);
+
+//3. Arrow Functions inherit the 'this' from it's enclosing context.
+let square = x => x * x; //Arrow Function 'square'
+let multiply = (a, b, c) => a*b*c; //Arrow Function 'multiply'
+
+//You call an arrow function like any other function.
+console.log(`square(9) = ${square(9)}`);
+console.log(`multiply(1,2,3) = ${multiply(1,2,3)}`);
+
+//Arrow functions are nameless. 
+console.log(square);
+console.log(multiply);
+
+//Arrow functions are important because they inherit the 'this' from its enclosing context.
+function declaredFunctionTest() { //Will print out NaN because a declared function doesn't inherit 'this'.
+    this.counter = 0;
+    function incrementCounter() {
+        console.log(`Inside the declared function`);
+        for (let i = 0; i < 10; i++) {
+            console.log(`declaredFunctionTest: Iteration ${i}: ${this.counter++}`);
+
+        }
+    }
+    incrementCounter();
+}
+let decTest = new declaredFunctionTest();
+
+function arrowFunctionTest() { //Will print out proper numbers because an arrow function will inherit the 'this' context.
+    this.counter = 0;
+    let incrementCounter = () => { //Arrow Function Declaration (multiple lines)
+        console.log(`Inside arrowFunctionTest`);
+        for (let i = 0; i < 10; i++) {
+            console.log(`arrowFunctionTest: Iteration ${i}: ${this.counter++}`);
+
+        }
+    }
+    incrementCounter();
+    console.log(this.counter);
+}
+let arrTest = new arrowFunctionTest();
+
+
+//Self-Invoking Functions
+console.log("Self invoking function syntax is to wrap a function in a set of parenthesis,", 
+"and have a trailing set of parenthesis for any parameter.");
+
+(function() {
+    console.log("I never explicitly invoked this function! :D");
+})();
+
+(function(b, a) {
+    console.log(`Self-invoking addition: ${a} + ${b} = ${a+b}`);
+})(10, 5);
+//You can put a self-invoking function inside of another function. 
+
+
+//Hoisted Function
+myHoistedFunction(141);
+
+function myHoistedFunction(line) {
+    console.log(`This function was implemented on lines 143 and 145, but invoked on line ${line}.`)
+}
+
+
+//Callback Functions
+function getName(callback) {
+    let name = prompt('What is your name?');
+    callback(name);
+}
+function printName(name) {
+    alert(`Your name is ${name}! Hello, ${name}!`);
+}
+//getName(printName);
+function multiplier(factor) {
+    return number => number * factor; //Short, 1-line Arrow Function
+}
+function otherMultiplier(factor) {
+    return number = (number) => {
+        return number * factor; //Multi-line Arrow Function
+    }
+}
+
+//triple = function 
+let triple = multiplier(3); //Functional programming is at work here.
+let longTriple = otherMultiplier(5); //Declares longTriple as a function variable. 
+console.log(`triple(9)=${triple(9)}`);
+console.log(`longTriple(3) = ${longTriple(3)}`);
--- JavaScript Assessment/src/Calculator.js
+++ JavaScript Assessment/src/Calculator.js
@@ -2,17 +2,38 @@ let Calculator = function() {
 
     /* Provide an implementation that sums the elements in an array */
     this.sum = function(array) {
+        console.log("Start of sum. array passed: ", array);
+        //Check if array is an array.
+        if (Array.isArray(array)) {
+            for (let i = 0; i < array.length; i++) { //Check each value of the array for non-numbers.
+                if (typeof(array[i]) !== 'number') {
+                    console.log("Error: not a number.\nEnd of Sum.");
+                    throw new Error();
+                }
+            }
+        }
+        else { //If array is not an array, throw an error. 
+            console.log("Error: Value passed is not an array.\nEnd of sum.") 
+            throw new Error(); 
+        }
+        console.log("No Errors Found. Proceeding with calculation.");
         let arraySum = 0;
         for (let i = 0; i < array.length; i++) {
             arraySum += array[i];
         }
+        console.log("Sum of ",array," is "+arraySum + "\nEnd of sum.");
         return arraySum;
     };
 
     /* Provide an implementation that finds the factorial of value (value!) */
     this.factorial = function(value) {
+        //Check if the value is a number, that's greater than or equal to zero, and if it's an integer.
+        if (typeof(value) !== 'number' || (value < 0) || (value - Math.trunc(value) != 0)) {
+            throw new Error();
+        }
+        if (value === 0) return 1; //If value is 0, return 1. 
         let f = 1;
-        for (let i = value; i !== 0; i--) {
+        for (let i = 1; i <= value; i++) { //Count up to value and multiply f each step of the way.
             f = f * i;
         }
         return f;
@@ -21,18 +42,40 @@ let Calculator = function() {
     /* Provide an implementation that returns true if a number is odd, and false if the number is even */
     /* CONDITION: You may not use the modulus (%) operator */
     this.isOdd = function(value) {
-        if ((value/2)*2 === value) {
-            console.log("Value/2 => ", value/2, "*2 = ", (value/2)*2);
-            return true;
+        if (typeof(value) !== 'number') { //If value is not a number, throw an error.
+            throw new Error();
         }
-        else return false;
+        else {
+            console.log("In isOdd, value="+value);
+            let div = Math.trunc(value/2); //Divide value by two, get rid of the decimal.
+            console.log("trunc(Value/2) => ", Math.trunc(value/2) + "\n"+div+"*2 === "+div*2);
+            if (div*2 === value) {
+                console.log("Value "+value+" evaluated to be even.");
+                return false;
+            }
+            else return true;
+        }
     }
 
     /* Provide an implementation that finds the nth term in the Fibonacci Sequence */
     /* HINT: The first two terms are 1 */
-    this.termInFibonacciSequence =  function(n) {
-        if (n <= 1) {return 1;}
-        else if (n === 2) {return 1;}
-        else return n+(termInFibonacciSequence(n-1)+termInFibonacciSequence(n-2));
+    this.termInFibonacciSequence =  function(n) { //TODO: Change to regular for loop. W/ - checking, recursion won't work.
+        console.log(`Entered Fibonacci. n=${n}.`);
+        if (typeof(n) !== 'number' || n < 0) {
+            console.log("Exiting Fibonacci. n isn't a positive number.");
+            throw new Error();
+        }
+        if (n === 0)  {
+            console.log("Reached start of Fibonacci sequence.");
+            return 0;
+        }
+        if (n === 1 || n === 2) {
+            console.log("Reached first two terms of Fibonacci sequence.");
+            return 1;
+        }
+        else {
+            if (n-2 < 0) return this.termInFibonacciSequence(n-1);
+            else return (this.termInFibonacciSequence(n-1)+this.termInFibonacciSequence(n-2));
+        }
     };
 };
