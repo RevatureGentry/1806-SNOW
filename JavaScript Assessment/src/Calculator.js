dc435feb6e559c473a1f8e6e49b538a1abc666ae
kchang0381 <kchang0381@gmail.com>
2018-06-19 20:28:19 -0400

    Finished homework assignment. All tests passed.

--- JavaScript Assessment/src/Calculator.js
+++ JavaScript Assessment/src/Calculator.js
@@ -2,23 +2,55 @@ let Calculator = function() {
 
     /* Provide an implementation that sums the elements in an array */
     this.sum = function(array) {
-        
+        var i;
+        let sum= 0;
+        for(i=0; i<array.length; i++){
+            if(isNaN(array[i]))
+                throw new Error("is not a number");
+            sum = array[i] + sum;
+        }
+        return sum;
     };
 
     /* Provide an implementation that finds the factorial of value (value!) */
     this.factorial = function(value) {
+        if(isNaN(value))
+            throw new Error("is not a number");
         
+        if(value < 0 || typeof(value) === "boolean" || value.constructor === Array)
+            throw new Error("invalid inputs");
+        let factorial = 1;
+        while (value>0){
+            factorial = factorial * value;
+            value--;
+        }
+        return factorial;
     }
 
     /* Provide an implementation that returns true if a number is odd, and false if the number is even */
     /* CONDITION: You may not use the modulus (%) operator */
     this.isOdd = function(value) {
-        
+        if(isNaN(value))
+            throw new Error("is not a number");
+    
+        if(value < 0 || typeof(value) === "boolean" || value.constructor === Symbol)
+            throw new Error("invalid inputs");
+        if(value & 1 === 1){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     /* Provide an implementation that finds the nth term in the Fibonacci Sequence */
     /* HINT: The first two terms are 1 */
     this.termInFibonacciSequence =  function(n) {
-        
+        if(n === 1 || n === 2){
+            return 1;
+        }
+        else{
+            return this.termInFibonacciSequence(n-1) + this.termInFibonacciSequence(n-2);
+        }
     };
 };
--- JavaScript Assessment/src/StringManipulator.js
+++ JavaScript Assessment/src/StringManipulator.js
@@ -3,23 +3,61 @@ function StringManipulator() {
     /* Provide an implementation that returns true if a string is a palindrome, and false if the string is not */
     /* CONDITION: you may not use String.prototype.reverse() */
     this.isPalindrome = function(value) {
-        
+        var check = value.replace(/\s/g, '');
+        var palindrome = '';
+        var i;
+        for(i=check.length-1; i>=0; i--){
+            palindrome += check.charAt(i);
+        }
+        //return check;
+        if(palindrome === check){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     /* Provide an implementation that reverses a string */
     /* CONDITION: you may only use String.prototype.charAt() */
     this.reverseString = function(value) {
-        
+        if(isNaN(value) === false || typeof(value) === "boolean" || typeof(value) === 'object')
+            throw new Error("invalid inputs");
+        var reversedString = '';
+        var i;
+        for(i = value.length-1; i>=0; i--){
+            reversedString += value.charAt(i);
+        }
+        return reversedString;
     }
 
     /* Provide an implementation that counts and returns the occurence of the letter "B" in a string */
     this.countBs = function(value) {
-
+        if(isNaN(value) === false || typeof(value) === "boolean" || typeof(value) === 'object')
+            throw new Error("invalid inputs");
+        var i;
+        let counter=0;
+        for(i=0; i<value.length; i++){
+            if(value[i]==='B'){
+                counter ++;
+            }
+        }
+        return counter;
     }
 
     /* An abstraction of the previous function, provide an implementation that counts and returns the  */
     /* Occurence of the letter 'char' in the String 'value' */
     this.countCharInString = function(value, char) {
-
+        if(typeof(value) !== 'string' || typeof(char) !== 'string')
+            throw new Error("invalid inputs");
+        var i;
+        let counter=0;
+        var regex = new RegExp(char, 'g');
+        var found = value.match(regex || []);
+        if(found === null){
+            return 0;
+        }
+        counter = found.length;
+        return counter;
     }
 }
