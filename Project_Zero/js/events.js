1f2e57526622219d131cce7e185778aa4a2bb52e
thomaslai113 <thomas.lai113@gmail.com>
2018-06-20 23:24:55 -0400

    Updated Project Zero, added grid layout, styling and basic features for a Whack-a-mole game

---
+++ Project_Zero/js/events.js
@@ -0,0 +1,90 @@
+var a = setInterval(randomPopUpTarget, 200);
+var b = setInterval(randomPopInTarget, 200);
+//var c = setInterval(randomizeHideDebuff, 1000);
+//var d = setInterval(randomizeTwiceBuff, 1000);
+var points = 0;
+var multiplier = 1;
+
+window.onload = function () {
+  let elements = document.getElementsByClassName("down");
+  for (let element of elements) {
+    element.addEventListener('click', () => {
+      if (element.classList.contains("up")) {
+        console.log("dopop");
+        element.classList.add("down");
+        element.classList.remove("up");
+        points += 100 * multiplier;
+        document.getElementById("scoreboard").innerText = "Score: " + points;
+      }
+      if (element.classList.contains("hider")) {
+        element.classList.add("down");
+        element.classList.remove("up");
+        document.getElementById("mainDiv").style.cursor = 'none';
+        setTimeout(() => {
+          document.getElementById("mainDiv").style.cursor = 'auto';
+        }, 7000);
+        multiplier = 1;
+        points -= 100;
+        document.getElementById("scoreboard").innerText = "Score: " + points;
+      }
+      if (element.classList.contains("twice")) {
+        element.classList.add("down");
+        element.classList.remove("up");
+        multiplier = multiplier * 2;
+        setTimeout(() => {
+          multiplier = 1;
+        }, 10000);
+        points += multiplier * 100;
+      }
+      document.getElementById("scoreboard").innerText = "Score: " + points;
+      /*else if(element.getAttribute("src")=="popin.png"){
+       element.style.paddingTop = '0px';
+       element.setAttribute("src","./assets/popup.png");
+       
+       }*/
+    })
+  }
+
+}
+
+function randomPopUpTarget() {
+
+
+  let array = document.getElementsByClassName("down");
+  var x = Math.floor((Math.random() * array.length) + 0);
+  let element = array[x];
+  element.classList.add("up");
+  element.classList.remove("down");
+
+
+}
+function randomPopInTarget() {
+
+  let array = document.getElementsByClassName("up");
+  var x = Math.floor((Math.random() * array.length - 1) + 0);
+  if (x != -1) {
+    let element = array[x];
+    element.classList.add("down");
+    element.classList.remove("hider");
+    element.classList.remove("up");
+  }
+
+}
+function randomizeHideDebuff() {
+  let array = document.getElementsByClassName("down");
+  var x = Math.floor((Math.random() * array.length) + 0);
+  let element = array[x];
+  element.setAttribute("src", "./assets/hide.png");
+  element.classList.add("up");
+  element.classList.add("hider");
+  element.classList.remove("down");
+}
+function randomizeTwiceBuff() {
+  let array = document.getElementsByClassName("down");
+  var x = Math.floor((Math.random() * array.length) + 0);
+  let element = array[x];
+  element.setAttribute("src", "./assets/poptwice.png");
+  element.classList.add("up");
+  element.classList.add("twice");
+  element.classList.remove("down");
+}
--- web_examples/events.js
+++ web_examples/events.js
@@ -1,5 +1,9 @@
-var a = setInterval(randomPopUpTarget,100);
-var b = setInterval(randomPopInTarget,100);
+var a = setInterval(randomPopUpTarget,200);
+var b = setInterval(randomPopInTarget,170);
+var c = setInterval(randomizeHideDebuff,500);
+var d = setInterval(randomizeTwiceBuff,500);
+var points = 0;
+var multiplier = 1;
 window.onload = function(){
 	let elements = document.getElementsByTagName("img");
 		
@@ -11,8 +15,30 @@ window.onload = function(){
 			element.setAttribute("src","popin.png");
 			element.classList.add("down");
 			element.classList.remove("up");
-			
+			points+=100*multiplier;
+			document.getElementById("scoreboard").innerText = "Score: " + points;
 			}
+			if(element.classList.contains("hider")){
+				element.style.paddingTop= '45px';
+			element.setAttribute("src","popin.png");
+			element.classList.add("down");
+			element.classList.remove("up");
+				document.getElementById("mainDiv").style.cursor = 'none';
+				setTimeout(()=>{document.getElementById("mainDiv").style.cursor = 'auto';},7000);
+				multiplier = 1;
+				points-=100;
+				document.getElementById("scoreboard").innerText = "Score: " + points;
+			}
+			if(element.classList.contains("twice")){
+				element.style.paddingTop= '45px';
+			element.setAttribute("src","popin.png");
+			element.classList.add("down");
+			element.classList.remove("up");
+			multiplier = multiplier*2;
+				setTimeout(()=>{multiplier = 1;},10000);
+				points += multiplier*100;
+			}
+			document.getElementById("scoreboard").innerText = "Score: " + points;
 			/*else if(element.getAttribute("src")=="popin.png"){
 				element.style.paddingTop = '0px';
 				element.setAttribute("src","popup.png");
@@ -40,9 +66,33 @@ function randomPopInTarget(){
 
 	let array = document.getElementsByClassName("up");
 	var x = Math.floor((Math.random() * array.length-1) + 0);
+	if(x != -1){
 	let element = array[x];
 	element.style.paddingTop = '45px';
 	element.setAttribute("src","popin.png");
 	element.classList.add("down");
+	element.classList.remove("hider");
 	element.classList.remove("up");
+	}
+	
+}
+function randomizeHideDebuff(){
+	let array = document.getElementsByClassName("down");
+	var x = Math.floor((Math.random() * array.length) + 0);
+	let element = array[x];
+	element.style.paddingTop = '0px';
+	element.setAttribute("src","hide.png");
+	element.classList.add("up");
+	element.classList.add("hider");
+	element.classList.remove("down");
+}
+function randomizeTwiceBuff(){
+	let array = document.getElementsByClassName("down");
+	var x = Math.floor((Math.random() * array.length) + 0);
+	let element = array[x];
+	element.style.paddingTop = '0px';
+	element.setAttribute("src","poptwice.png");
+	element.classList.add("up");
+	element.classList.add("twice");
+	element.classList.remove("down");
 }
