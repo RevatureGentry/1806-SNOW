000a57a20f8ba1dc7f39ef3a01e715c87553f97a
skhan73370 <skhan73370@gmail.com>
2018-06-21 09:01:22 -0400

    Implemented functions in Calculator.js and String Manipulator.js

--- JavaScript Assessment/src/Calculator.js
+++ JavaScript Assessment/src/Calculator.js
@@ -2,23 +2,53 @@ let Calculator = function() {
 
     /* Provide an implementation that sums the elements in an array */
     this.sum = function(array) {
-        
+        array.forEach(element => {
+            if(typeof(element) !== 'number'){
+                throw new Error (element +" is not a number");
+            }
+        });
+        let sum = 0;
+        for(let i = 0;i < array.length;i++){
+            sum += array[i];
+        }
+
+        return sum;
     };
 
     /* Provide an implementation that finds the factorial of value (value!) */
     this.factorial = function(value) {
-        
+        if(typeof(value) !== 'number' ||value < 0){
+            throw new Error(`${value} is not a valid input`);
+        }
+
+        if(value === 1 || value === 0){
+            return 1;
+        }else{
+            return value * this.factorial(value-1);
+        }
+
     }
 
     /* Provide an implementation that returns true if a number is odd, and false if the number is even */
     /* CONDITION: You may not use the modulus (%) operator */
     this.isOdd = function(value) {
-        
+        if(typeof(value) !== 'number'){
+            throw new Error(`${value} is not a valid input`);
+        }
+        let binary = value.toString(2);
+        return binary.charAt(binary.length-1) === '1';
     }
 
     /* Provide an implementation that finds the nth term in the Fibonacci Sequence */
     /* HINT: The first two terms are 1 */
     this.termInFibonacciSequence =  function(n) {
-        
+        if(typeof(n) !== 'number' ||n < 0){
+            throw new Error(`${n} is not a valid input`);
+        }
+        if(n <=2 ){
+            return 1;
+        }else{
+            return this.termInFibonacciSequence(n-1)+ this.termInFibonacciSequence(n-2);
+        }
     };
 };
--- JavaScript Assessment/src/StringManipulator.js
+++ JavaScript Assessment/src/StringManipulator.js
@@ -3,23 +3,77 @@ function StringManipulator() {
     /* Provide an implementation that returns true if a string is a palindrome, and false if the string is not */
     /* CONDITION: you may not use String.prototype.reverse() */
     this.isPalindrome = function(value) {
-        
+        if(typeof(value) !== 'string'){
+            throw new Error("Invalid input");
+        }
+        let words = value.split(" ").join("");
+        let reverse = "";
+        for(letter of words){
+             reverse = letter + reverse;
+        }
+        return reverse === words;
+
     }
 
     /* Provide an implementation that reverses a string */
     /* CONDITION: you may only use String.prototype.charAt() */
     this.reverseString = function(value) {
-        
+        if(typeof(value) !== 'string'){
+            throw new Error("Invalid input");
+        }
+        let reverseString = "";
+        let i = 0;
+        while(value.charAt(i) !== ""){
+            i++;
+        }
+        for(i;i >=0;i--){
+            reverseString += value.charAt(i);
+        }
+        return reverseString
     }
 
     /* Provide an implementation that counts and returns the occurence of the letter "B" in a string */
     this.countBs = function(value) {
-
+        if(typeof(value) !== 'string'){
+            throw new Error("Invalid input");
+        }
+        let sum = 0;
+        for(letter of value){
+           if(letter === 'B'){
+               sum++;
+           } 
+        }
+        return sum;
     }
 
     /* An abstraction of the previous function, provide an implementation that counts and returns the  */
     /* Occurence of the letter 'char' in the String 'value' */
     this.countCharInString = function(value, char) {
-
+        if(typeof(value) !== 'string' || typeof(char) !== 'string'){
+            throw new Error("Invalid input");
+        }
+        let words = value.split(" ");
+        
+        let j = 0;
+        let lastPos = 0;
+        let sum = 0
+        for(word of words){
+            for(let i = 0;i <word.length;i++){
+                if(word.charAt(i) === char.charAt(j)){
+                    j++;
+                    if(j === char.length){
+                        sum++;
+                        j = 0;
+                        lastPos = i;
+                    }
+                }else{
+                    lastPos++;
+                    j = 0;
+                }
+            }
+            lastPos = 0;
+            j = 0;
+        }
+        return sum;
     }
 }
