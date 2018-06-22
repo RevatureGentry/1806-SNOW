9913c5dd850eca85c2fbb4fc30f3926372e14536
[Cody Dotson] <[Dotson_60@hotmail.com]>
2018-06-22 08:57:28 -0400

    changes

--- JavaScript Assessment/src/Calculator.js
+++ JavaScript Assessment/src/Calculator.js
@@ -3,22 +3,72 @@ let Calculator = function() {
     /* Provide an implementation that sums the elements in an array */
     this.sum = function(array) {
         
-    };
+            
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        if(typeof (array.reduce(reducer)) === "number") {
+            let answer =(array.reduce(reducer));
+            return answer;
+        }
+        else{
+            throw new Error;
+        }
+    }
+    
 
     /* Provide an implementation that finds the factorial of value (value!) */
     this.factorial = function(value) {
-        
+
+        if(typeof value ==="number" && value >= 0){
+        var result = value;
+        if (value === 0 || value === 1){ 
+            return 1; 
+        }
+        while (value > 1) { 
+            value--;
+            result *= value;
+        }
+            return result;
+        }
+        else{
+            throw new Error;
+        }
     }
 
     /* Provide an implementation that returns true if a number is odd, and false if the number is even */
     /* CONDITION: You may not use the modulus (%) operator */
     this.isOdd = function(value) {
         
+        if(typeof value ==="number"){
+    
+        
+            if((value & 1) == 1){
+                return true;
+            }
+            else{
+                return false;
+            }
+                }
+        else{
+            throw new Error;
+        }
     }
 
     /* Provide an implementation that finds the nth term in the Fibonacci Sequence */
     /* HINT: The first two terms are 1 */
     this.termInFibonacciSequence =  function(n) {
-        
-    };
+
+    if(typeof n === "number" && n>=0){
+    var a = 1, b = 0, temp;
+    while (n >= 1){
+        temp = a;
+        a = a + b;
+        b = temp;
+        n--;
+    }
+    return b;
+    }
+    else{
+        throw new Error;
+    }        
+    }
 };
--- JavaScript Assessment/src/StringManipulator.js
+++ JavaScript Assessment/src/StringManipulator.js
@@ -3,23 +3,83 @@ function StringManipulator() {
     /* Provide an implementation that returns true if a string is a palindrome, and false if the string is not */
     /* CONDITION: you may not use String.prototype.reverse() */
     this.isPalindrome = function(value) {
-        
-    }
 
+
+        //str represents the 'value' with spaces eliminated using the replace method
+        let str = value.replace(/\s+/g, '');
+
+        if(typeof str==="string"){
+            var len = Math.floor(str.length / 2);
+            for (var i = 0; i < len; i++){
+                if (str[i] !== str[str.length - i - 1]){
+                    return false;
+                }
+                
+                else{
+                    return true;
+                }
+            }
+        }
+        else {
+            throw new Error;
+        }         
+}
+
+        
     /* Provide an implementation that reverses a string */
     /* CONDITION: you may only use String.prototype.charAt() */
     this.reverseString = function(value) {
+        var index = 0;
         
-    }
+        if(typeof value === "string"){
+            while(value.charAt(index) != "") {
+                value.charAt(index);
+                console.log(value.charAt(index));
+                index++;
+                var length = index;
+            }
+            var ReversedString = "";
+            for (var i = length - 1; i >= 0; i--) {
+                ReversedString = ReversedString + value[i];
+            }
+            console.log(ReversedString);
+            return ReversedString;
+    
+        }   
+        else{
+            throw new Error;
+        }
+}
+
+        
+    
 
     /* Provide an implementation that counts and returns the occurence of the letter "B" in a string */
     this.countBs = function(value) {
 
+        //if statement to determine if 'value' is of string type
+        if(typeof value==="string"){
+            //statement to return # of occurences of letter "B"
+            return (value.split("B").length-1) //4
+        }
+        //else if to throw error if 'variable' is other type than string
+        else {
+            throw new Error;
+        }
     }
 
     /* An abstraction of the previous function, provide an implementation that counts and returns the  */
     /* Occurence of the letter 'char' in the String 'value' */
     this.countCharInString = function(value, char) {
 
+        //If statement to determine if both variables are of string type
+        if(typeof value, typeof char==="string"){
+            //statement to return the number of occurences of 'char' in 'value'
+            return (value.split(char).length-1) //4
+        }
+        //else if to throw error if variables are of other type than string
+        else {
+            throw new Error;
+        }
     }
 }
