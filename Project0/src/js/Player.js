9bc2cc2900584d332ef000a11fad49b514a62828
chenhowa <hchen7913@gmail.com>
2018-06-23 09:21:22 -0400

    Tested a player for Tone.js

--- Project0/src/js/Player.js
+++ Project0/src/js/Player.js
@@ -1,12 +1,94 @@
 
-
+import * as types from "./typecheck";
 
 // This class is reponsible for taking a lexed and translated
-// *.music file and playing it in the browser.
+// *.music file and playing it in the browser. Therefore it will utilize Tone.js
 
 class Player {
     constructor() {
+        this._instruments = [];
+        //this._instrument = new Tone.Synth().toMaster();
+        //this._instrument_2 = new Tone.Synth().toMaster();
+    }
 
+    /*
+        example notes:
+        [  // array of voices.
+            [   // each voice is an array of notes and durations to play them.
+                ["A4", "B4"],
+                [1, 2], // The intention is to play notes in sequence.
+            ],
+            [
+                ["C4", "D4"],
+                [3, 5],
+
+                // Note that voices do not play at identical times. "D4" will start
+                // when "B4" ends. That is how the scheduling works.
+            ]
+        ]
+    */
+    schedule(notes) {
+        for(let i = 0; i < notes.length; i++) {
+            if(notes[i][0].length !== notes[i][1].length) {
+                throw new Error("number of notes does not equal number of durations");
+            }
+            // add an instrument for each voice.
+            this._instruments.push(new Tone.Synth({
+                "oscillator" : {
+                    "type" : "amtriangle",
+                    "harmonicity" : 0.5,
+                    "modulationType" : "sine"
+                },
+                "envelope" : {
+                    "attackCurve" : 'exponential',
+                    "attack" : 0.05,
+                    "decay" : 0.2,
+                    "sustain" : 0.2,
+                    "release" : 1.5,
+                },
+                "portamento" : 0.05
+            }).toMaster());
+
+            //for each note-duration pair, schedule the note with correct duration.
+            let overall_time = 0;
+            let notes_array = notes[i][0];
+            let dur_array = notes[i][1];
+            for(let noteIndex = 0; noteIndex < notes_array.length; noteIndex++) {
+                Tone.Transport.schedule(
+                    this._scheduleNote(notes_array[noteIndex], dur_array[noteIndex]),
+                    overall_time
+                );
+
+                // Increment the overall_time for the scheduling of the next note.
+                overall_time += dur_array[noteIndex];
+            }
+        }
+    }
+
+    _scheduleNote(note, duration) {
+        types.typecheckString(note);
+        types.typecheckNumber(duration);
+
+        let instrument = this._instruments[this._instruments.length - 1];
+        return function playNote(time) {
+            instrument.triggerAttackRelease(note, duration, time);
+        }
+    }
+
+    play() {
+        Tone.Transport.start('+0.1');
+
+        /*
+        let instrument = this._instrument;
+        let instrument_2 = this._instrument_2;
+        return function doPlay(time) {
+            instrument_2.triggerAttackRelease("G4", "3", time);
+            instrument.triggerAttackRelease("C4", "3", time);
+        }*/
+    }
+
+    stop() {
+        Tone.Transport.stop();
     }
 }
 
--- Project0/src/js/testing.js
+++ Project0/src/js/testing.js
@@ -3,12 +3,27 @@ import Player from "./Player";
 window.onload = function(event) {
 
     console.log("I am running");
-
-    const player = new Player();
-    player.play([
+    let player = new Player();
+    player.schedule([
         [
-            ["A4", "B4", "C4", "D4", "E4", "F4", "G4"],
-            ["0.25m", "0.25m", "0.25m", "0.25m", "0.25m", "0.25m", "0.25m"],
-        ]
+            ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"],
+            [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+        ],
+        /*[
+            ["E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5"],
+            [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+        ]*/
     ]);
+    const playBtn = document.getElementById("play");
+    const stopBtn = document.getElementById("stop");
+    playBtn.addEventListener("click", (event) => {
+        player.play();
+        playBtn.disabled = true;
+        stopBtn.disabled = false;
+    });
+    stopBtn.addEventListener("click", (event) => {
+        player.stop();
+        playBtn.disabled = false;
+        stopBtn.disabled = true;
+    })
 }
