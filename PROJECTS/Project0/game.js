fd8c4e67cef37c3f5f9827e41861875769c98d51
Cullen Martell <cullenjmartell@gmail.com>
2018-07-05 09:07:16 -0400

    Final version for presentation push. lets hope its a good presentation.

--- PROJECTS/Project0/game.js
+++ PROJECTS/Project0/game.js
@@ -1,4 +1,4 @@
-//Card Deck
+//card image array
 const cardsArray = [{
     'name' : 'stein',
     'img' : './cards/beer-stein.png',
@@ -59,30 +59,41 @@ const cardsArray = [{
     'name' : 'wolf',
     'img' : './cards/wolf-head.png',
 }
-];
+]; //15 unique cards = 30 displayed cards
 
-const gameGrid = cardsArray
-  .concat(cardsArray)
-  .sort(() => 0.5 - Math.random());
+//this will create a new array with 2x the amount of images
+//from before to make matches
+const gameGrid = cardsArray.concat(cardsArray).sort(() => 0.5 - Math.random());
 
-let firstGuess = '';
+let firstGuess = '';    // need first and second to allow for multiple guesses
 let secondGuess = '';
 let count = 0;
-let previousTarget = null;
-let delay = 1200;
+let previousTarget = null;  //created to prevent players from selecting the same element twice
+let delay = 1000;
 
-const game = document.getElementById('game');
+const game = document.getElementById('game'); //grab the div with id 'game'
 const grid = document.createElement('section');
-grid.setAttribute('class', 'grid');
-game.appendChild(grid);
+grid.setAttribute('class', 'grid');       //create a new section with class grid
+game.appendChild(grid);               // add grid to the game div
 
+/* 
+function countclicks {
+
+} 
+*/
+function reload() {
+  location.reload();
+}
+
+
+// for each card array item
 gameGrid.forEach(item => {
   const { name, img } = item;
 
   const card = document.createElement('div');
   card.classList.add('card');
   card.dataset.name = name;
-
+// create the blank front of the card
   const front = document.createElement('div');
   front.classList.add('front');
 
@@ -95,12 +106,14 @@ gameGrid.forEach(item => {
   card.appendChild(back);
 });
 
+//when matched add css 
 const match = () => {
   const selected = document.querySelectorAll('.selected');
   selected.forEach(card => {
     card.classList.add('match');
   });
 };
+
 
 const resetGuesses = () => {
   firstGuess = '';
@@ -114,10 +127,11 @@ const resetGuesses = () => {
   });
 };
 
+//whenever an image is clicked, select it
 grid.addEventListener('click', event => {
 
   const clicked = event.target;
-
+// do not allow the background, previous card or matches to become selected
   if (
     clicked.nodeName === 'SECTION' ||
     clicked === previousTarget ||
@@ -130,15 +144,18 @@ grid.addEventListener('click', event => {
   if (count < 2) {
     count++;
     if (count === 1) {
+      //become first guess
       firstGuess = clicked.parentNode.dataset.name;
       console.log(firstGuess);
       clicked.parentNode.classList.add('selected');
     } else {
+      //become second guess
       secondGuess = clicked.parentNode.dataset.name;
       console.log(secondGuess);
       clicked.parentNode.classList.add('selected');
     }
 
+    //if both guesses aren't empty and they match, we run match and reset guess attempts
     if (firstGuess && secondGuess) {
       if (firstGuess === secondGuess) {
         setTimeout(match, delay);
@@ -147,6 +164,4 @@ grid.addEventListener('click', event => {
     }
     previousTarget = clicked;
   }
-
 });
-
